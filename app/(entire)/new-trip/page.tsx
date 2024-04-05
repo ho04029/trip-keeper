5e512dbc5fbb833e213a0e76d2988846a8de0dea
ho04029 <ho04029@naver.com>
2024-04-05 12:18:12 +0900

    new-trip 페이지 zod

--- app/(entire)/new-trip/page.tsx
+++ app/(entire)/new-trip/page.tsx
@@ -1,4 +1,13 @@
-import React from "react";
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z.string(),
+  date: z.string(),
+  userId: z.string(),
+  schedule: z.array(z.string()),
+  isArchived: z.boolean(),
+  isPublished: z.boolean(),
+});
 
 const NewTrip = () => {
   return <main></main>;

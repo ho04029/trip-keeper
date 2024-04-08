f0378b21ffbe1c36ccee6e07c68e36d1fe02cf70
ho04029 <ho04029@naver.com>
2024-04-08 16:26:18 +0900

    feat: convex

--- app/(entire)/new-trip/page.tsx
+++ app/(entire)/new-trip/page.tsx
@@ -2,29 +2,43 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
 const formSchema = z.object({
   title: z.string(),
   date: z.string(),
-  userId: z.string(),
   schedule: z.array(z.string()),
-  isArchived: z.boolean(),
   isPublished: z.boolean(),
 });
 
-export function TripForm() {
+const NewTrip = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      isArchived: false,
       isPublished: false,
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {}
-}
-
-const NewTrip = () => {
-  return <main></main>;
+  const onSubmit = (values: z.infer<typeof formSchema>) => {};
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField />
+        </form>
+      </Form>
+    </>
+  );
 };
 
 export default NewTrip;
---
+++ convex/documents.ts
@@ -0,0 +1,31 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const createNewTrip = mutation({
+  args: {
+    title: v.string(),
+    date: v.string(),
+    schedule: v.array(v.string()),
+    isPublished: v.boolean(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    const trip = await ctx.db.insert("trip", {
+      title: args.title,
+      date: args.date,
+      userId,
+      schedule: args.schedule,
+      isPublished: args.isPublished,
+      isArchived: false,
+    });
+
+    return trip;
+  },
+});

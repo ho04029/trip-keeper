440ec083d4e1b68df2fb87ce5a2d9b0658c21edc
ho04029 <ho04029@naver.com>
2024-04-08 13:50:27 +0900

    feat: new-trip 페이지

--- app/(entire)/new-trip/page.tsx
+++ app/(entire)/new-trip/page.tsx
@@ -1,4 +1,6 @@
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 const formSchema = z.object({
   title: z.string(),
@@ -8,6 +10,18 @@ const formSchema = z.object({
   isArchived: z.boolean(),
   isPublished: z.boolean(),
 });
+
+export function TripForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      isArchived: false,
+      isPublished: false,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {}
+}
 
 const NewTrip = () => {
   return <main></main>;

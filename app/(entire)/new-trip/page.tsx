fd0ff609a33860808f83efccbf3451d77524fea8
ho04029 <ho04029@naver.com>
2024-04-09 13:34:01 +0900

    feat: new-trip페이지

--- app/(entire)/new-trip/page.tsx
+++ app/(entire)/new-trip/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -6,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,7 +16,7 @@ import {
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-  title: z.string(),
+  title: z.string().min(2, { message: "최소 2글자 이상을 입력해주세요" }),
   date: z.string(),
   schedule: z.array(z.string()),
   isPublished: z.boolean(),
@@ -25,19 +26,36 @@ const NewTrip = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      title: "",
       isPublished: false,
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {};
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log("here");
+    console.log(values);
+  };
+
   return (
-    <>
+    <main className="absolute top-10">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField />
+          <FormField
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>여행 제목</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">저장</Button>
         </form>
       </Form>
-    </>
+    </main>
   );
 };
 

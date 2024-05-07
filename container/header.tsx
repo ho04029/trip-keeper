ca3f5768dd17ac96689f0c4a582bec9a0e6e4203
ho04029 <ho04029@naver.com>
2024-05-07 14:42:06 +0900

    feat: 헤더 상단 고정

--- container/header.tsx
+++ container/header.tsx
@@ -2,14 +2,22 @@ import { useConvexAuth } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
 import Link from "next/link";
 
+import useScrollTop from "@/hooks/use-scroll-top";
 import ModeToggle from "@/components/ui/mode-toggle";
 import { Button } from "@/components/ui/button";
 import Spinner from "@/components/spinner";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
   const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
   return (
-    <header className="w-full relative top-0 left-0 flex justify-between p-1 px-5 bg-white dark:bg-black">
+    <header
+      className={cn(
+        "z-10 w-full fixed top-0 left-0 flex justify-between p-1 px-5 bg-white dark:bg-black",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
       <div className="flex items-center">
         <Link href="/" className="font-medium">
           Trip keeper
